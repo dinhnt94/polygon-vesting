d0627e14a6d3cef461ae3887eef30c2a3b9403f2
nhapq <nhapham@kardiachain.io>
2021-09-04 01:37:53 +0700

    init

---
+++ contracts/Token.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+//import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v4.1/contracts/token/ERC20/ERC20.sol"; // this for remix
+
+
+contract BOMBC is ERC20 {
+    address public owner;
+    
+    constructor() payable ERC20("BOMBC Token", "BOMBC") {
+        owner = msg.sender;
+    }
+
+    function mint(address account, uint256 amount) public {
+        require(msg.sender==owner, "!owner"); 
+        _mint(account, amount);
+    }
+}
