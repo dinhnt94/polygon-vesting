84f98b0396d1e465345543066285daae19c192b0
nhatnn1996 <newbie.dev.js@gmail.com>
2021-09-21 14:38:58 +0700

    fisrt commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import Contract from "./context/contract";
 import Address from "./context/address";
 import Tool from "./views/tool";
-import Clarm from "./views/claim";
-import Home from "./views/index";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "antd/dist/antd.css";
 import "./App.css";
 function App() {
   return (
     <Contract>
-      <Router>
-        <Switch>
-          {/* <Route exact path="/tool">
-            <Address>
-              <Tool />
-            </Address>
-          </Route> */}
-
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
+      <Address>
+        <Tool />
+      </Address>
     </Contract>
   );
 }
--- src/views/tool.js
+++ src/views/tool.js
@@ -1,49 +1,49 @@
-// import Upload from "../components/Upload/index";
-// import { Row, Col, notification } from "antd";
-// import Table from "../components/Table";
-// import FormAdd from "../components/Form";
-// import { useEffect } from "react";
-// import { useContract } from "../hooks/contract";
+import Upload from "../components/Upload/index";
+import { Row, Col, notification } from "antd";
+import Table from "../components/Table";
+import FormAdd from "../components/Form";
+import { useEffect } from "react";
+import { useContract } from "../hooks/contract";
 
-// const App = () => {
-//   const { connectBcoin } = useContract();
-//   useEffect(() => {
-//     const init = async () => {
-//       const result = await connectBcoin();
-//       if (result && result.account) {
-//         if (result && result.account) {
-//           notification.success({
-//             message: "Connect to Metamask wallet success"
-//           });
-//         } else {
-//           notification.error({
-//             message: "Not Found Metamask"
-//           });
-//         }
-//       }
-//     };
-//     init();
-//   }, []);
+const App = () => {
+  const { connectBcoin } = useContract();
+  useEffect(() => {
+    const init = async () => {
+      const result = await connectBcoin();
+      if (result && result.account) {
+        if (result && result.account) {
+          notification.success({
+            message: "Connect to Metamask wallet success"
+          });
+        } else {
+          notification.error({
+            message: "Not Found Metamask"
+          });
+        }
+      }
+    };
+    init();
+  }, []);
 
-//   return (
-//     <div className="container">
-//       <Row gutter={[20, 20]} style={{ marginTop: 100 }}>
-//         <Col span="20">
-//           <FormAdd />
-//         </Col>
-//       </Row>
-//       <Row>
-//         <Col style={{ marginTop: 50 }} span={12}>
-//           <Upload />
-//         </Col>
-//       </Row>
-//       <Row>
-//         <Col style={{ marginTop: 50 }} span={24}>
-//           <Table />
-//         </Col>
-//       </Row>
-//     </div>
-//   );
-// };
+  return (
+    <div className="container">
+      <Row gutter={[20, 20]} style={{ marginTop: 100 }}>
+        <Col span="20">
+          <FormAdd />
+        </Col>
+      </Row>
+      <Row>
+        <Col style={{ marginTop: 50 }} span={12}>
+          <Upload />
+        </Col>
+      </Row>
+      <Row>
+        <Col style={{ marginTop: 50 }} span={24}>
+          <Table />
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
-// export default App;
+export default App;
