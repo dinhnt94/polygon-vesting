8194b99d8b1515b394255f82d36f6504b9e359fb
Dinh Nguyen <dinhnt@senspark.com>
2022-06-26 11:12:52 +0700

    Update smc, to adapt quickly we reuse and added
    Signed-off-by: Dinh Nguyen <dinhnt@senspark.com>

--- contracts/BcoinVesting.sol
+++ contracts/BcoinVesting.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+<<<<<<< Updated upstream
 import "./IERC20.sol";
 
 /**
@@ -181,6 +182,194 @@ abstract contract Context {
         this;
         return msg.data;
     }
+=======
+import "./BEP20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+abstract contract BCoinVesting is Ownable, AccessControl {
+  using SafeMath for uint256;
+
+  // Address of BCOIN Token.
+  IBEP20 public bcoinToken;
+
+  // Starting timestamp of vesting
+  // Will be used as a starting point for all dates calculations.
+  // The first vesting will happen one month after this timestamp
+  uint256 public vestingStartAt;
+
+  // Vesting duration in seconds
+  uint256 public vestingDuration;
+
+  // Vesting cliff is one month
+  // 365*(60*60*24) / 12
+  uint256 internal constant SECONDS_PER_MONTH = 2628000;
+
+  // Percent of vested token which can be withraw per month;
+  uint256 internal percent_unlease_per_month;
+
+  // Beneficiary contains details of each beneficiary/investor
+  struct Beneficiary {
+    uint256 initialBalance;
+    uint256 monthsClaimed;
+    uint256 totalClaimed;
+    uint256 magicPercent;
+  }
+
+  // Create a new role identifier for the minter role
+  bytes32 public constant VESTING_ADMIN_ROLE = keccak256("VESTING_ADMIN_ROLE");
+
+  // beneficiaries tracks all beneficiary and store data in storage
+  mapping(address => Beneficiary) public beneficiaries;
+
+  // Event raised on each successful withdraw.
+  event Claim(address beneficiary, uint256 amount, uint256 timestamp);
+
+  // Event raised on each desposit
+  event Deposit(address beneficiary, uint256 initialBalance, uint256 timestamp);
+
+  // @dev constructor creates the vesting contract
+  // @param _token Address of BCOIN token
+  // @param _owner Address of owner of this contract, a.k.a the CEO
+  // @param _vestingStartAt the starting timestamp of vesting , in seconds.
+  // @param _vestingDuration the duration since _vestingStartAt until the vesting ends, in months.
+  constructor(
+    address _token,
+    address _owner,
+    uint256 _vestingStartAt,
+    uint256 _vestingDuration
+  ) {
+    require(_token != address(0), "zero-address");
+    require(_owner != address(0), "zero-address");
+    bcoinToken = IBEP20(_token);
+    transferOwnership(_owner);
+    vestingStartAt = _vestingStartAt;
+    vestingDuration = _vestingDuration;
+    // Grant the minter role to a specified account
+    _setupRole(VESTING_ADMIN_ROLE, _owner);
+    _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+  }
+
+  // @dev addBeneficiary registers a beneficiary and deposit a
+  // corresponded amount of token for this beneficiary
+  //
+  // The owner can call this function many times to update
+  // (additionally desposit) the amount of token for this beneficiary
+  // @param _beneficiary Address of the beneficiary
+  // @param _amount Amount of token belongs to this beneficiary
+  function addBeneficiary(address _beneficiary, uint256 _amount) public onlyOwner() {
+    //require(block.timestamp < vestingStartAt, "not-update-after-vesting-started");
+    require(_beneficiary != address(0), "zero-address");
+    // Based on ERC20 standard, to transfer funds to this contract,
+    // the owner must first call approve() to allow to transfer token to this contract.
+    require(bcoinToken.transferFrom(_msgSender(), address(this), _amount), "cannot-transfer-token-to-this-contract");
+
+    // update storage data
+    Beneficiary storage bf = beneficiaries[_beneficiary];
+    bf.initialBalance = bf.initialBalance.add(_amount);
+
+    emit Deposit(_beneficiary, bf.initialBalance, block.timestamp);
+  }
+
+  // Do more adding
+  function addWhitelists(address[] memory users, uint256[] memory amounts) public onlyOwner() {
+      require(users.length == amounts.length, "set-diff-len");
+      for (uint i = 0; i < users.length; i++) {
+          addBeneficiary(users[i], amounts[i]);
+      }
+  }
+
+  // Owner can nerf user when user is blacklist
+  function nerfUsers(address user, uint percentage) public onlyOwner() {
+    // update storage data
+    Beneficiary storage bf = beneficiaries[user];
+    bf.magicPercent = percentage;
+  }
+  
+  // Reduce balance user when wrong/inflate
+  function reduceInitBalance(address user, uint reduceNum) public onlyOwner() {
+    // update storage data
+    Beneficiary storage bf = beneficiaries[user];
+    bf.initialBalance = bf.initialBalance.sub(reduceNum);
+  }
+
+  // @dev Claim withraws the vested token and sends beneficiary
+  // Only the owner or the beneficiary can call this function
+  // @param _beneficiary Address of the beneficiary
+  function claimVestedToken(address _beneficiary) public {
+    require( (owner() == msg.sender) || (_msgSender() == _beneficiary), "must-be-onwer-or-beneficiary");
+    uint256 monthsVestable;
+    uint256 tokenVestable;
+    (monthsVestable, tokenVestable) = calculateClaimable(_beneficiary);
+    require(tokenVestable > 0, "nothing-to-be-vested");
+
+    // update data in blockchain storage
+    Beneficiary storage bf = beneficiaries[_beneficiary];
+    bf.monthsClaimed = bf.monthsClaimed.add(monthsVestable);
+    bf.totalClaimed = bf.totalClaimed.add(tokenVestable);
+
+    // check magic
+    uint256 amount = tokenVestable.sub(tokenVestable.mul(bf.magicPercent).div(100));
+    require(bcoinToken.transfer(_beneficiary, amount), "fail-to-transfer-token");
+
+    emit Claim(_beneficiary, tokenVestable, block.timestamp);
+  }
+
+  // calculateWithrawable calculates the claimable token of the beneficiary
+  // claimable token each month is rounded if it is a decimal number
+  // So the rest of the token will be claimed on the last month (the duration is over)
+  // @param _beneficiary Address of the beneficiary
+  function calculateClaimable(address _beneficiary) private view returns (uint256, uint256) {
+    uint256 _now = block.timestamp;
+    if (_now < vestingStartAt) {
+      return (0, 0);
+    }
+
+    uint256 elapsedTime = _now.sub(vestingStartAt);
+    uint256 elapsedMonths = elapsedTime.div(SECONDS_PER_MONTH);
+
+    if (elapsedMonths < 1) {
+      return (0, 0);
+    }
+
+    Beneficiary storage bf = beneficiaries[_beneficiary];
+    require(bf.initialBalance > 0, "beneficiary-not-found");
+
+    // If over vesting duration, all tokens vested
+    if (elapsedMonths >= vestingDuration) {
+      uint256 remaining = bf.initialBalance.sub(bf.totalClaimed);
+      return (vestingDuration, remaining);
+    } else {
+      uint256 monthsVestable = elapsedMonths.sub(bf.monthsClaimed);
+      uint256 tokenVestedPerMonth = bf.initialBalance.div(vestingDuration);
+      uint256 tokenVestable = monthsVestable.mul(tokenVestedPerMonth);
+      return (monthsVestable, tokenVestable);
+    }
+  }
+
+  // view function to check status of a beneficiary
+  function getBeneficiary(address _beneficiary)
+    public
+    view
+    returns (
+      uint256 initialBalance,
+      uint256 monthsClaimed,
+      uint256 totalClaimed
+    )
+  {
+    Beneficiary storage bf = beneficiaries[_beneficiary];
+    require(bf.initialBalance > 0, "beneficiary-not-found");
+
+    return (bf.initialBalance, bf.monthsClaimed, bf.totalClaimed);
+  }
+
+  //for emegency
+  function revokeFund() external onlyRole(VESTING_ADMIN_ROLE) {
+    uint256 amount = bcoinToken.balanceOf(address(this));
+    require(amount > 0, "fund-is-0");
+    require(bcoinToken.transfer(msg.sender, amount), "fail-to-revoke");
+  }
+>>>>>>> Stashed changes
 }
 
 /**
--- contracts/BcoinVestingFactory.sol
+++ contracts/BcoinVestingFactory.sol
@@ -5,6 +5,7 @@ import "./BcoinVesting.sol";
 
 
 /**
+<<<<<<< Updated upstream
  * @dev PrivateSaleBcoinVesting will be blocked and release 10% each month.
  * Hence, the vestingDuration should be 10 months from the beginning.
  * 
@@ -14,10 +15,37 @@ contract PrivateSaleBcoinVesting is BcoinVesting {
                 address _owner, 
                 uint256 _vestingStartAt
                 ) BcoinVesting(_token, _owner, _vestingStartAt, 10) {}
+=======
+ * @dev PrivateSaleBCoinVesting will be blocked and release 5% each month.
+ * Hence, the vestingDuration should be 20 months from the beginning.
+ * BUT, return 5% after 1 day from TGE -> _vestingStartAt - (1 month + 1day)
+ */
+contract PrivateSaleBCoinVesting is BCoinVesting {
+  constructor(
+    address _token,
+    address _owner,
+    uint256 _vestingStartAt
+  ) BCoinVesting(_token, _owner, (_vestingStartAt - (2628000 + 24 * 3600)), 20) {}
+}
+
+/**
+ * @dev EcoFunBCoinVesting will be blocked 1 month and release 5% each month.
+ * Hence, the vestingDuration should be 20 months from the beginning.
+ *
+ */
+contract EcoFunBCoinVesting is BCoinVesting {
+  //uint256 private SECONDS_PER_YEAR = 31536000;
+  constructor(
+    address _token,
+    address _owner,
+    uint256 _vestingStartAt
+  ) BCoinVesting(_token, _owner, (_vestingStartAt + 2628000), 20) {}
+>>>>>>> Stashed changes
 }
 
 
 /**
+<<<<<<< Updated upstream
  * @dev PublicSaleBcoinVesting will be releaseed linearly each month during the next year.
  * Hence, the vestingDuration should be 12 months.
  * 
@@ -58,6 +86,36 @@ contract AdvisorBcoinVesting is BcoinVesting {
                 address _owner, 
                 uint256 _vestingStartAt
                 ) BcoinVesting(_token, _owner, (_vestingStartAt + 31536000) , 12) {}
+=======
+ * @dev TeamBCoinVesting will be blocked for 1 year, then release 5% each month.
+ * then releaseed linearly each month during the next year.
+ * Hence, the _vestingStartAt should delay 1 year
+ * and the vestingDuration should be 20 months.
+ *
+ */
+contract TeamBCoinVesting is BCoinVesting {
+  //uint256 private SECONDS_PER_YEAR = 31536000;
+  constructor(
+    address _token,
+    address _owner,
+    uint256 _vestingStartAt
+  ) BCoinVesting(_token, _owner, (_vestingStartAt + 31536000), 20) {}
+}
+
+/**
+ * @dev MarketingBCoinVesting will be release 5% each month.
+ * then releaseed linearly each month during the next year.
+ * and the vestingDuration should be 20 months.
+ *
+ */
+contract MarketingBCoinVesting is BCoinVesting {
+  //uint256 private SECONDS_PER_YEAR = 31536000;
+  constructor(
+    address _token,
+    address _owner,
+    uint256 _vestingStartAt
+  ) BCoinVesting(_token, _owner, (_vestingStartAt + 2628000), 20) {}
+>>>>>>> Stashed changes
 }
 
 /**
@@ -76,6 +134,7 @@ contract DexLiquidityBcoinVesting is BcoinVesting {
 }
 
 /**
+<<<<<<< Updated upstream
  * @dev ReserveBcoinVesting will be blocked for 1 year,
  * then releaseed linearly each month during the next 2 year.
  * Hence, the _vestingStartAt should delay 1 year 
@@ -88,6 +147,19 @@ contract ReserveBcoinVesting is BcoinVesting {
                 address _owner, 
                 uint256 _vestingStartAt
                 ) BcoinVesting(_token, _owner, (_vestingStartAt + 31536000) , 24) {}
+=======
+ * @dev ReserveBCoinVesting will be unlock when the pool reward runs out,
+ * Hence, the _vestingStartAt should now, and 1 month for all.
+ *
+ */
+contract ReserveBCoinVesting is BCoinVesting {
+  //uint256 private SECONDS_PER_YEAR = 31536000;
+  constructor(
+    address _token,
+    address _owner,
+    uint256 _vestingStartAt
+  ) BCoinVesting(_token, _owner, _vestingStartAt, 1) {}
+>>>>>>> Stashed changes
 }
 
 
@@ -95,6 +167,7 @@ contract ReserveBcoinVesting is BcoinVesting {
  * @dev BcoinVestingFactory is the main and is the only contract should be deployed.
  * Notice: remember to config the Token address and approriate startAtTimeStamp
  */
+<<<<<<< Updated upstream
 contract BcoinVestingFactory {
     // put the token address here
     // This should be included in the contract for transparency 
@@ -137,3 +210,57 @@ contract BcoinVestingFactory {
     }
     
 }
+=======
+contract BCoinVestingFactory {
+  // put the token address here
+  // This should be included in the contract for transparency
+  address public BCOIN_TOKEN_ADDRESS = 0xfda1003A66692bf5E6d947ED9f648F9BA265d04f;
+
+  // put the startAtTimeStamp here
+  // To test all contracts, change this timestamp to time in the past.
+  uint256 public startAtTimeStamp = 1656216479;
+
+  // address to track other information
+  address public owner;
+  address public privateSaleBCoinVesting;
+  address public teamBCoinVesting;
+  address public ecosystemBCoinVesting;
+  address public marketingBCoinVesting;
+  address public dexLiquidityBCoinVesting;
+  address public reserveBCoinVesting;
+
+  constructor() {
+    owner = msg.sender;
+
+    PrivateSaleBCoinVesting _privateSaleBCoinVesting = new PrivateSaleBCoinVesting(
+      BCOIN_TOKEN_ADDRESS,
+      owner,
+      startAtTimeStamp
+    );
+    privateSaleBCoinVesting = address(_privateSaleBCoinVesting);
+
+    TeamBCoinVesting _teamBCoinVesting = new TeamBCoinVesting(BCOIN_TOKEN_ADDRESS, owner, startAtTimeStamp);
+    teamBCoinVesting = address(_teamBCoinVesting);
+
+    EcoFunBCoinVesting _ecoBCoinVesting = new EcoFunBCoinVesting(BCOIN_TOKEN_ADDRESS, owner, startAtTimeStamp);
+    ecosystemBCoinVesting = address(_ecoBCoinVesting);
+
+    MarketingBCoinVesting _marketingBCoinVesting = new MarketingBCoinVesting(
+      BCOIN_TOKEN_ADDRESS,
+      owner,
+      startAtTimeStamp
+    );
+    marketingBCoinVesting = address(_marketingBCoinVesting);
+
+    DexLiquidityBCoinVesting _dexLiquidityBCoinVesting = new DexLiquidityBCoinVesting(
+      BCOIN_TOKEN_ADDRESS,
+      owner,
+      startAtTimeStamp
+    );
+    dexLiquidityBCoinVesting = address(_dexLiquidityBCoinVesting);
+
+    ReserveBCoinVesting _reserveBCoinVesting = new ReserveBCoinVesting(BCOIN_TOKEN_ADDRESS, owner, startAtTimeStamp);
+    reserveBCoinVesting = address(_reserveBCoinVesting);
+  }
+}
+>>>>>>> Stashed changes
