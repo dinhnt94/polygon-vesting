9e69b336318b9ff0dd83f9fb387264a5c65da002
Dinh Nguyen <dinhnt@senspark.com>
2022-06-26 11:21:04 +0700

    Update
    Signed-off-by: Dinh Nguyen <dinhnt@senspark.com>

--- contracts/BcoinVestingFactory.sol
+++ contracts/BcoinVestingFactory.sol
@@ -3,19 +3,7 @@ pragma solidity ^0.8.0;
 
 import "./BcoinVesting.sol";
 
-
 /**
-<<<<<<< Updated upstream
- * @dev PrivateSaleBcoinVesting will be blocked and release 10% each month.
- * Hence, the vestingDuration should be 10 months from the beginning.
- * 
- */
-contract PrivateSaleBcoinVesting is BcoinVesting {
-    constructor(address _token,
-                address _owner, 
-                uint256 _vestingStartAt
-                ) BcoinVesting(_token, _owner, _vestingStartAt, 10) {}
-=======
  * @dev PrivateSaleBCoinVesting will be blocked and release 5% each month.
  * Hence, the vestingDuration should be 20 months from the beginning.
  * BUT, return 5% after 1 day from TGE -> _vestingStartAt - (1 month + 1day)
@@ -40,53 +28,9 @@ contract EcoFunBCoinVesting is BCoinVesting {
     address _owner,
     uint256 _vestingStartAt
   ) BCoinVesting(_token, _owner, (_vestingStartAt + 2628000), 20) {}
->>>>>>> Stashed changes
-}
-
-
-/**
-<<<<<<< Updated upstream
- * @dev PublicSaleBcoinVesting will be releaseed linearly each month during the next year.
- * Hence, the vestingDuration should be 12 months.
- * 
- */
-contract PublicSaleBcoinVesting is BcoinVesting {
-    constructor(address _token,
-                address _owner, 
-                uint256 _vestingStartAt
-                ) BcoinVesting(_token, _owner, _vestingStartAt , 12) {}
-}
-
-
-/**
- * @dev TeamBcoinVesting will be blocked for 1 year,
- * then releaseed linearly each month during the next year.
- * Hence, the _vestingStartAt should delay 1 year 
- * and the vestingDuration should be 12 months.
- * 
- */
-contract TeamBcoinVesting is BcoinVesting {
-    //uint256 private SECONDS_PER_YEAR = 31536000;
-    constructor(address _token,
-                address _owner, 
-                uint256 _vestingStartAt
-                ) BcoinVesting(_token, _owner, (_vestingStartAt + 31536000) , 12) {}
 }
 
 /**
- * @dev AdvisorBcoinVesting will be blocked for 1 year,
- * then releaseed linearly each month during the next year.
- * Hence, the _vestingStartAt should delay 1 year 
- * and the vestingDuration should be 12 months.
- * 
- */
-contract AdvisorBcoinVesting is BcoinVesting {
-    //uint256 private SECONDS_PER_YEAR = 31536000;
-    constructor(address _token,
-                address _owner, 
-                uint256 _vestingStartAt
-                ) BcoinVesting(_token, _owner, (_vestingStartAt + 31536000) , 12) {}
-=======
  * @dev TeamBCoinVesting will be blocked for 1 year, then release 5% each month.
  * then releaseed linearly each month during the next year.
  * Hence, the _vestingStartAt should delay 1 year
@@ -115,39 +59,25 @@ contract MarketingBCoinVesting is BCoinVesting {
     address _owner,
     uint256 _vestingStartAt
   ) BCoinVesting(_token, _owner, (_vestingStartAt + 2628000), 20) {}
->>>>>>> Stashed changes
 }
 
 /**
- * @dev DexLiquidityBcoinVesting will be blocked for 1 month,
+ * @dev DexLiquidityBCoinVesting will be blocked for 1 month,
  * then releaseed 5% each month during the next year.
- * Hence, the _vestingStartAt should delay 1 month 
+ * Hence, the _vestingStartAt should delay 1 month
  * and the vestingDuration should be 20 months.
- * 
+ *
  */
-contract DexLiquidityBcoinVesting is BcoinVesting {
-    //uint256 private SECONDS_PER_MONTH = 2628000;
-    constructor(address _token,
-                address _owner, 
-                uint256 _vestingStartAt
-                ) BcoinVesting(_token, _owner, (_vestingStartAt + 2628000) , 20) {}
+contract DexLiquidityBCoinVesting is BCoinVesting {
+  //uint256 private SECONDS_PER_MONTH = 2628000;
+  constructor(
+    address _token,
+    address _owner,
+    uint256 _vestingStartAt
+  ) BCoinVesting(_token, _owner, (_vestingStartAt + 2628000), 20) {}
 }
 
 /**
-<<<<<<< Updated upstream
- * @dev ReserveBcoinVesting will be blocked for 1 year,
- * then releaseed linearly each month during the next 2 year.
- * Hence, the _vestingStartAt should delay 1 year 
- * and the vestingDuration should be 24 months.
- * 
- */
-contract ReserveBcoinVesting is BcoinVesting {
-    //uint256 private SECONDS_PER_YEAR = 31536000;
-    constructor(address _token,
-                address _owner, 
-                uint256 _vestingStartAt
-                ) BcoinVesting(_token, _owner, (_vestingStartAt + 31536000) , 24) {}
-=======
  * @dev ReserveBCoinVesting will be unlock when the pool reward runs out,
  * Hence, the _vestingStartAt should now, and 1 month for all.
  *
@@ -159,58 +89,12 @@ contract ReserveBCoinVesting is BCoinVesting {
     address _owner,
     uint256 _vestingStartAt
   ) BCoinVesting(_token, _owner, _vestingStartAt, 1) {}
->>>>>>> Stashed changes
 }
-
 
 /**
- * @dev BcoinVestingFactory is the main and is the only contract should be deployed.
+ * @dev BCoinVestingFactory is the main and is the only contract should be deployed.
  * Notice: remember to config the Token address and approriate startAtTimeStamp
  */
-<<<<<<< Updated upstream
-contract BcoinVestingFactory {
-    // put the token address here
-    // This should be included in the contract for transparency 
-    address public BCOIN_TOKEN_ADDRESS = 0xd9145CCE52D386f254917e481eB44e9943F39138;
-    
-    // put the startAtTimeStamp here
-    // To test all contracts, change this timestamp to time in the past.
-    uint256 public startAtTimeStamp = 1624681036;
-    
-    // address to track other information
-    address public owner;
-    address public privateSaleBcoinVesting;
-    address public publicSaleBcoinVesting;
-    address public teamBcoinVesting;
-    address public advisorBcoinVesting;
-    address public dexLiquidityBcoinVesting;
-    address public reserveBcoinVesting;
-    
-    constructor() {
-        owner = msg.sender;
-        
-        PrivateSaleBcoinVesting _privateSaleBcoinVesting = new PrivateSaleBcoinVesting(BCOIN_TOKEN_ADDRESS, owner, startAtTimeStamp);
-        privateSaleBcoinVesting = address(_privateSaleBcoinVesting);
-        
-        PublicSaleBcoinVesting _publicSaleBcoinVesting = new PublicSaleBcoinVesting(BCOIN_TOKEN_ADDRESS, owner, startAtTimeStamp);
-        publicSaleBcoinVesting = address(_publicSaleBcoinVesting);
-        
-        TeamBcoinVesting _teamBcoinVesting = new TeamBcoinVesting(BCOIN_TOKEN_ADDRESS, owner, startAtTimeStamp);
-        teamBcoinVesting = address(_teamBcoinVesting);
-        
-        AdvisorBcoinVesting _advisorBcoinVesting = new AdvisorBcoinVesting(BCOIN_TOKEN_ADDRESS, owner, startAtTimeStamp);
-        advisorBcoinVesting = address(_advisorBcoinVesting);
-        
-        DexLiquidityBcoinVesting _dexLiquidityBcoinVesting = new DexLiquidityBcoinVesting(BCOIN_TOKEN_ADDRESS, owner, startAtTimeStamp);
-        dexLiquidityBcoinVesting = address(_dexLiquidityBcoinVesting);
-        
-        ReserveBcoinVesting _reserveBcoinVesting = new ReserveBcoinVesting(BCOIN_TOKEN_ADDRESS, owner, startAtTimeStamp);
-        reserveBcoinVesting = address(_reserveBcoinVesting);
-        
-    }
-    
-}
-=======
 contract BCoinVestingFactory {
   // put the token address here
   // This should be included in the contract for transparency
@@ -263,4 +147,3 @@ contract BCoinVestingFactory {
     reserveBCoinVesting = address(_reserveBCoinVesting);
   }
 }
->>>>>>> Stashed changes
